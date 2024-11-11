e34f33043f6c0cb02a8044b3f07e6f46119a1435
Dmitriy <archangel2903@gmail.com>
2024-11-11 16:10:46 +0200

    fix navigation

--- src/js/index.js
+++ src/js/index.js
@@ -36,6 +36,23 @@ $(function () {
     window.addEventListener('scroll', adjustHeaderOnScroll);
     window.addEventListener('load', adjustHeaderOnScroll);
 
+    // Anchor scroll
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+
     // Swiper slider
     /*if ($('.swiper-container').length) {
         let slider;
